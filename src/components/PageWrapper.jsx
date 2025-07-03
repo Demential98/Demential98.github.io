1f30b2b18200e2a1d45d9cdbff022baafea44e02
Demential98 <92541225+Demential98@users.noreply.github.com>
2025-07-03 17:56:34 +0200

    Update PageWrapper.jsx

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -2,17 +2,17 @@ import { motion } from 'framer-motion';
 
 export default function PageWrapper({ children }) {
   return (
-    // <motion.div
-    //   className="h-full flex flex-col"
-    //   initial={{ opacity: 0, y: 30 }}
-    //   animate={{ opacity: 1, y: 0 }}
-    //   exit={{ opacity: 0, y: -30 }}
-    //   transition={{ duration: 0.4, ease: [0.25, 0.1, 0.25, 1] }}
-    // >
-    //   {children}
-    // </motion.div>
-    <div className="h-full flex flex-col">
+    <motion.div
+      className="h-full flex flex-col"
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -30 }}
+      transition={{ duration: 0.4, ease: [0.25, 0.1, 0.25, 1] }}
+      >
       {children}
-    </div>
+    </motion.div>
+    // <div className="h-full flex flex-col">
+    //  {children}
+    // </div>
   );
 }
